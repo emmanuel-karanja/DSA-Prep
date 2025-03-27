d45dfdda63bda01c31d0bd8a60b3d55678d9e8c2
emmanuel-karanja <emmanuel.nganga.ke@gmail.com>
2025-03-27 13:57:38 +0300

    move zeroes

---
+++ Arrays/maxConsecutiveZeros.js
@@ -0,0 +1,32 @@
+/**iven a binary array nums (only 0s and 1s), 
+ * and an integer k, return the length of the longest subarray that contains at most k zeroes. 
+ * 
+ * LOGIC:
+ * 
+ Use a sliding window to track a subarray, and only allow at most k zeroes in the current window.
+
+Expand the right boundary (right) of the window
+
+Count the number of zeroes inside the window
+
+If zeroes > k, shrink from the left (left) until valid again
+ * */
+
+function longestOnes(nums, k) {
+    let left = 0, right = 0;
+    let maxLen = 0;
+    let zeroCount = 0;
+
+    for (right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) zeroCount++;
+
+        while (zeroCount > k) {
+            if (nums[left] === 0) zeroCount--;
+            left++;
+        }
+
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+}
---
+++ Arrays/moveZeroes.js
@@ -0,0 +1,31 @@
+/**Given a binary array move zeroes to one side in O(n)
+ * 
+ * LOGIC:\
+Instead of pushing zeroes to the end (which is common), this time we want them at the beginning. The idea is:
+
+Traverse the array from end to start
+
+Copy non-zero elements to the back of the array (from the last index)
+
+Fill the remaining positions with 0
+ */
+
+function moveZeroesToStart(arr) {
+    let n = arr.length;
+    let write = n - 1;
+
+    // Step 1: Write non-zeroes from end to start
+    for (let i = n - 1; i >= 0; i--) {
+        if (arr[i] !== 0) {
+            arr[write] = arr[i];
+            write--;
+        }
+    }
+
+    // Step 2: Fill the rest with zeroes at the beginning
+    for (let i = 0; i <= write; i++) {
+        arr[i] = 0;
+    }
+
+    return arr;
+}
