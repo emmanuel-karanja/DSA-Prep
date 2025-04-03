1d576b4e3bc5d50ba0beae41e6d2184e3dd8165e
Emmanuel Ng'ang'a <60355631+emmanuel-karanja@users.noreply.github.com>
2025-04-03 22:03:08 +0300

    understand right-left-1

--- Strings/longestPalindrome.js
+++ Strings/longestPalindrome.js
@@ -10,42 +10,43 @@
  *  */
 
 function longestPalindrome(s) {
-    //edge case where the s has a single char
-    if(s.length===1) return s;
+  if (s.length <= 1) return s; // Edge case: single character or empty string
 
-     let maxLength=0;
+  let maxLength = 0;
+  let start = 0;
 
-     let start=0;
+  // Helper function to expand around a center
+  function expandAroundCenter(left, right) {
+      while (left >= 0 && right < s.length && s[left] === s[right]) {
+          left--; 
+          right++; 
+      }
+      //we do right-left-1 since the checking stops when left and right are too far left.
+      //normally, we'd do right-left+1 to get length including, but since now left and right are a step
+      //too far left and right, it'd be right-left+1-2 = right-left-1
+      return right - left - 1; // Length of the palindrome
+  }
 
-   //helper to expand around center
-   function expandAroundCenter(left,right){
-     
-    //we are within bounds and have a palindrome
-     if(left>0 && right < s.length && s[left]===s[right]){
-         left--; right++; //expand
-     }
-     return right-left-1; //return the length of the current palindrome
-   }
+  for (let i = 0; i < s.length; i++) {
+      let oddLength = expandAroundCenter(i, i); // Odd-length palindrome
+      let evenLength = expandAroundCenter(i, i + 1); // Even-length palindrome
 
-    //expand around each char
-   for(let i=0;i<s.length-1;i++){
-      let oddLength=expandAroundCenter(i,i); //odd
-      let evenLength=expandAroundCenter(i,i+1); //even
-      let currentLength=0;
-      currentLength=Math.max(oddLength,evenLength)
+      let currentLength = Math.max(oddLength, evenLength);
 
-     if(currentLength > maxLength){
-         maxLength=currentLength;
-         start=i - Math.floor((len - 1) / 2);
-     }
-   }
+      if (currentLength > maxLength) {
+          maxLength = currentLength;
+          //because i is the center of the palindrome and we need to find the beginning of it, removing 1 caters
+          //for odd lengths
+          start = i - Math.floor((currentLength - 1) / 2);
+      }
+  }
 
-   return s.substring(start, start + maxLength)
-   
+  return s.substring(start, start + maxLength);
 }
 
-//Time O(N^2) 
-
-// Test
+// Test cases
 console.log(longestPalindrome("babad")); // Output: "bab" or "aba"
 console.log(longestPalindrome("cbbd"));  // Output: "bb"
+console.log(longestPalindrome("a"));     // Output: "a"
+console.log(longestPalindrome("racecar")); // Output: "racecar"
+console.log(longestPalindrome("abcd"));  // Output: "a" (or any single character)
